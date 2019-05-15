bf8d313fed0eef799fe7e4232842d97942da911a
Cristofher <cristofher@C.local>
2019-05-15 04:39:45 -0400

    updated main.cpp licence to MIT

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 /* Copyright (C) 2019 Cristian Zambrano - All Rights Reserved
  * You may use, distribute and modify this code under the
- * terms of the XYZ license, which unfortunately won't be
- * written for another century.
- *
- * You should have received a copy of the XYZ license with
- * this file. If not, please write to: , or visit :
+ * terms of the MIT license.
  */
 
 #include "game.h"
