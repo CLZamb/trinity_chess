38c161314bd701a8fc68577221df42e86dc11bed
CLZamb <Cristianlzambrano@outlook.com>
2023-02-26 18:53:47 -0500

    improved text and arrow input, change options to only show end, begin constant pointer

--- src/piece/pawn.cpp
+++ src/piece/pawn.cpp
@@ -38,7 +38,7 @@ U64 WhitePawn::pawn_non_attack(int sq) {
   U64 forward_two_sq = BLANK;
 
   forward_one_sq = shift<NORTH>(from_sq);
-  forward_two_sq |= shift<NORTH>(forward_one_sq & ROWMASK[2]) ;
+  forward_two_sq |= shift<NORTH>(forward_one_sq & ROWMASK[2]);
   result = forward_one_sq | forward_two_sq;
   return result;
 }
